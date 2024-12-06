bd5ae7721389f24bd469aca9630f0b0dd5cce355
nasdashti <155775216+nasdashti@users.noreply.github.com>
2024-12-06 19:16:10 +0700

    Update youtube.ts

--- src/types/youtube.ts
+++ src/types/youtube.ts
@@ -1,23 +1,3 @@
-export interface Video {
-  id: string;
-  title: string;
-  thumbnailUrl: string;
-  publishedAt: string;
-  url: string;
-}
+import { Video, Channel } from './channel';
 
-export interface Channel {
-  id: string;
-  handle: string;
-  name: string;
-  thumbnailUrl: string;
-  description: string;
-  url: string;
-  latestVideo: Video | null;
-  streak: {
-    current: number;
-    longest: number;
-    lastUploadDate: string;
-    startDate: string;
-  };
-}
+export type { Video, Channel };
